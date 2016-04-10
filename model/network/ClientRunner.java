a8727f78380e12cc17f18de0c17a29018e3bf188
John Caingles <johncaingles@gmail.com>
2016-04-11 01:43:32 +0800

    cleaned

--- model/network/ClientRunner.java
+++ model/network/ClientRunner.java
@@ -9,32 +9,12 @@ import model.AppDatabase;
 import model.Transaction;
 
 public class ClientRunner implements Runnable {
-	
-	private Socket socketToHost;
-	
-	public ClientRunner(){}
-	public ClientRunner(Socket socketToHost){
-		this.socketToHost = socketToHost;
-	}
 
 	@Override
 	public void run() {
 		try {
-	        // Create the socket
-//	        socketToHost = new Socket(hostIpAddress, portNum);
-	        // Create the input & output streams to the server
-//	        ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
-	        /** --------------------------- USEFULL SHIT YUNG SA TAAS ---------------------- **/
-//	        ObjectInputStream inFromServer = new ObjectInputStream(socketToHost.getInputStream());
-
-	        /* Send the Message Object to the server */
-//	        outToServer.writeObject(msgList);            
-	        /** --------------------------- USEFULL SHIT YUNG SA TAAS ---------------------- **/
-	        
-//	        Transaction msgFrmServer = (Transaction)inFromServer.readObject();
-//	        System.out.println(msgFrmServer.getQueryStatement());
-//	        socketToHost.close();
-
+            // haha whut
+            // dapat runnable/ thread pa rin daw siya in case of multiple sendings
 	    } catch (Exception e) {
 	        System.err.println("Client Error: " + e.getMessage());
 	        System.err.println("Localized: " + e.getLocalizedMessage());
--- model/network/ServerRunner.java
+++ model/network/ServerRunner.java
@@ -14,12 +14,7 @@ public class ServerRunner implements Runnable {
 
 	private ServerSocket serverSocket;
 	private PeerToPeerDBController controller;
-	
-	public ServerRunner(){}
-	public ServerRunner(ServerSocket serverSocket) {
-		this.serverSocket = serverSocket;
-	}
-	
+
 	public ServerRunner(ServerSocket serverSocket, PeerToPeerDBController controller) {
 		this.serverSocket = serverSocket;
 		this.controller = controller;
@@ -31,31 +26,13 @@ public class ServerRunner implements Runnable {
         	while(true){
 				// Create the Client Socket FOR ACCEPTING
 				Socket clientSocket = serverSocket.accept();
-				System.out.println("Socket Extablished... basta received somethings");
-				System.out.println(clientSocket.getRemoteSocketAddress());
-				// Create input and output streams to client
-
-				/** use me */
-				//ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
 
 				ObjectInputStream inFromClient = new ObjectInputStream(clientSocket.getInputStream());
 
-				/* Create Message object and retrive information */
-	//            LinkedList<Message> inList = new LinkedList<>();
-	//            Message inMsg = null;
-	//            inList = (LinkedList<Message>)inFromClient.readObject();
 				Transaction receivedTransaction = (Transaction)inFromClient.readObject();
 				if(receivedTransaction != null){
 					controller.receiveTransaction(receivedTransaction);
 				}
-//				System.out.println("I RECEIVED: "+ receivedTransaction.getQueryStatement());
-	//            inList = (LinkedList<Message>)inFromClient.readObject();
-	//            inMsg = inList.pop();
-				/* Send the modified Message object back */
-				/** use me */
-				//Transaction receivedTransaction = new Transaction("sup", "niggah");
-				/** use me */
-				//outToClient.writeObject(receivedTransaction);
         	}
 	    } catch (Exception e) {
 	        System.err.println("Server Error: " + e.getMessage());
